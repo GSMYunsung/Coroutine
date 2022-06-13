0b9e6c6acfa45cfd7a4437506b6ea310542158f9
choiyunsung <12345ggh81@gmail.com>
2022-06-13 21:07:23 +0900

    [UPDATE] 쿼리 추가

--- app/src/main/java/com/yunsung/coroutine/util/Utils.kt
+++ app/src/main/java/com/yunsung/coroutine/util/Utils.kt
@@ -1,5 +1,14 @@
 package com.yunsung.coroutine.util
 
 object Utils {
-    const val BASE_URL = "https://api.giphy.com"
+    const val BASE_URL = "https://apis.data.go.kr/6290000/msmartglrbaseinfo/"
+    const val apiKey = "u4AoHlpli2eYsQ7hGGzjoWO4Im9iktqI8f2OT6Yzl4d3cHAAoaWXBaXxrhfdAp8ESXDn7%2BuL8KB1JcKqEZsY1g%3D%3D"
+    const val QUERY_API_KEY = "serviceKey"
+    const val QUERY_NUM_OF_ROWS = "numOfRows"
+    const val QUERY_PAGE_NO = "pageNo"
+    const val QUERY_TYPE = "type"
+    const val QUERY_OPERATION_RULE_NUM = "operationRuleNm"
+
+
+
 }
