6a34e4c677b202a44442112a498aa9d9e88257c2
choiyunsung <12345ggh81@gmail.com>
2022-06-27 21:11:38 +0900

    [UPDATE] 이름변경 및 구성요소 추가

--- app/src/main/java/com/yunsung/coroutine/data/naverdata/local/NaverDao.kt
+++ app/src/main/java/com/yunsung/coroutine/data/naverdata/local/NaverDao.kt
@@ -1,13 +1,18 @@
 package com.yunsung.coroutine.data.naverdata.local
 
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
-import androidx.room.Update
+import androidx.room.*
 
+@Dao
 interface NaverDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insert(naverNewsEntity: naverNewsEntity)
+    suspend fun insertNews(naverNewsEntity: naverNewsEntity)
+
+    @Query("SELECT * FROM naverNewsEntity WHERE naverNewsTitle = :naverNewsTitle AND naverNewsContent = :naverNewsContent ANd naverLink = :naverLink")
+    suspend fun getNews(naverNewsTitle : String, naverNewsContent : String, naverLink : String) : naverNewsEntity
+
+    @Delete
+    suspend fun deleteNews (newsEntity: naverNewsEntity)
 
 
 }
--- app/src/main/java/com/yunsung/coroutine/data/naverdata/local/naverNewsEntity.kt
+++ app/src/main/java/com/yunsung/coroutine/data/naverdata/local/naverNewsEntity.kt
@@ -6,7 +6,8 @@ import androidx.room.PrimaryKey
 @Entity
 data class naverNewsEntity(
     var naverNewsTitle : String,
-    var naverNewsContent : String
+    var naverNewsContent : String,
+    var naverLink : String
 ){
     @PrimaryKey(autoGenerate = true)
     var id = 0
